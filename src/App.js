75634297cd1cd45b1efdea587dd2fc78b6d45d01
Youngjae <yj122198@naver.com>
2020-10-02 00:40:04 +0900

    before start movie_app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  constructor(props) {
-    super(props);
-    console.log("strat");
-  }
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
+
   componentDidMount() {
-    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-  componentDidUpdate() {
-    console.log("component updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, world");
-  }
+
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "we are ready"}</div>;
   }
 }
 
